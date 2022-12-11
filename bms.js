690b60054f2cdd7fbb84dd104290fca610d06fb0
PramodK2808 <pramodkushwaha1328@gmail.com>
2022-12-11 11:04:37 +0530

    first commit

---
+++ bms.js
@@ -0,0 +1,9 @@
+function scroll(){
+    var left = document.querySelectorAll(".scroll-img");
+    left.scrollBy(350,0)
+}
+
+function scrollr(){
+    var right = document.querySelectorAll(".scroll-img");
+    right.scrollBy(350,0)
+}
