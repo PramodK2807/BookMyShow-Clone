f758bc1e57bab13711d802c57abc42a837d74962
PramodK2808 <pramodkushwaha1328@gmail.com>
2022-12-13 11:17:02 +0530

    code

--- bms.js
+++
@@ -1,9 +0,0 @@
-function scroll(){
-    var left = document.querySelectorAll(".scroll-img");
-    left.scrollBy(350,0)
-}
-
-function scrollr(){
-    var right = document.querySelectorAll(".scroll-img");
-    right.scrollBy(350,0)
-}
