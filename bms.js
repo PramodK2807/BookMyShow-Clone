3c1d82872630055b99d467dff4054252807931a8
PramodK2808 <pramodkushwaha1328@gmail.com>
2022-12-28 13:29:29 +0530

    code

--- bms.js
+++ bms.js
@@ -1,31 +1,28 @@
 
-// function loadCoupon() {
-//     document.getElementsByClassName("coupon")[0].style.display = "block";
-//     document.querySelector("body").style.opacity = "0.5";
-// }
-
-
-// const closeCoupon = () => {
-//     document.getElementsByClassName("coupon")[0].style.display = "none";
-//     document.querySelector(".carousel").style.opacity = "1";
-// }
-
-// window.load = loadCoupon();
-
-
-// const darkMode = document.querySelector(".dm");
 
 function darkMode() {
     let myBody = document.body;
     myBody.classList.toggle("dark-mode");
-
-    // if (darkMode.classList.contains("fa-moon")) {
-    //     darkMode.classList.replace("fa-moon", "fa-sun");
-    //     darkMode.classList.add("fa-sun");
-    // }
-
-    // else {
-    //     darkMode.classList.replace("fa-sun", "fa-moon");
-    //     darkMode.classList.add("fa-moon");
-    // }
 }
+
+const bodyEl = document.querySelector("body");
+
+const closeCouponEl = document.querySelector(".closeBtn");
+const couponEl = document.querySelector(".coupon");
+
+
+function closeCoupon(){
+    couponEl.style.display = "none";
+    document.querySelector("body").style.opacity = "1";
+}
+
+window.onload = function(){
+    setTimeout(function couponLoad(){
+        couponEl.style.display = "block";
+        document.querySelector("body").style.opacity = "0.8";
+        document.querySelector(".coupon").style.opacity = "1";
+    }, 3000);
+   };
+
+
+
